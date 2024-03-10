901c61b914161381ea5bb317f82f641d6537d8bc
tictacthomas <157897317+tictacthomas@users.noreply.github.com>
2024-03-10 23:47:16 +0100

    Delete jan24ft-fet-ca-tictacthomas-main directory

--- jan24ft-fet-ca-tictacthomas-main/js/accessibility.js
+++
@@ -1,9 +0,0 @@
-$('input[id=accessToggle]').change(function() {
-    if ($(this).is(':checked')) {
-        $("p").css("font-size", "120%");
-    } else {
-        $("p").css("font-size", "100%");
-    }
-  });
-
-
