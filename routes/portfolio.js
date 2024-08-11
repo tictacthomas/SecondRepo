dbcf312bffc159b6cf9fe6b905c4e53a8be122d5
Thomas Roscher <roscher1993@gmail.com>
2024-08-12 01:54:16 +0200

    ok2

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
+const fs = require("fs")
+const path = require("path")
+var bodyParser = require('body-parser')
+var jsonParser = bodyParser.json()
 
-/* GET home page. */
+
+var request = require('request');
+
+//download image to the server:
+var download = function(url, filename, callback){
+  request.head(url, function(err, res, body){
+    request(url).pipe(fs.createWriteStream(path.resolve(__dirname, '../data/img/'+ filename))).on('close', callback);
+  });
+};
+
 router.get('/', function(req, res, next) {
-  res.render('portfolio', { title: 'Express' });
+  let data = fs.readFileSync(path.resolve(__dirname, "../data/portfolio.json"));
+  res.render('portfolio', { cakes: JSON.parse(data)});
+});
+
+
+router.post('/', jsonParser, function(req, res, next) {
+  let rawdata = fs.readFileSync(path.resolve(__dirname, "../data/portfolio.json"));
+  let portfoliosArray = JSON.parse(rawdata);
+  if(portfoliosArray.filter(x => x.name === req.body.name).length == 0) {
+    download(req.body.url, req.body.name, function(){
+      console.log('done');
+    });
+    const newArray = portfoliosArray.concat([req.body])
+    fs.writeFileSync(path.resolve(__dirname, "../data/portfolio.json"), JSON.stringify(newArray));
+  }
+  res.end();
 });
 
 module.exports = router;
